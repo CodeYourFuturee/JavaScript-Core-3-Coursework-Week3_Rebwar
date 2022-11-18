04789c759845289cbebfdd2601402dbbab3b616f
rebwar <rebwar1.azizi@gmail.com>
2022-11-18 19:31:12 +0000

    add jest test & exercises/exercise-1/exercise-1.

--- exercises/exercise-1/exercise-1.js
+++ exercises/exercise-1/exercise-1.js
@@ -4,10 +4,17 @@ const personOne = {
   favouriteFood: "Spinach",
 };
 
-function introduceYourself(___________________________) {
+function introduceYourself({ name, age, favouriteFood }) {
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
+  return `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`;
 }
 
 introduceYourself(personOne);
+
+test("should return a string with the name, age and favourite food", () => {
+  expect(introduceYourself(personOne)).toEqual(
+    "Hello, my name is Popeye. I am 34 years old and my favourite food is Spinach."
+  );
+});
